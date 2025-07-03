850a42794354bcc309efd39adffa635bc8251a06
Alexandre-Passos <alexandre15passos@gmail.com>
2025-07-03 17:40:01 -0300

    corrijindo o recebimento de erros

--- src/middleware/recebendoErros.js
+++ src/middleware/recebendoErros.js
@@ -2,7 +2,7 @@ import express from "express"
 import mongoose from "mongoose"
 
 const recebendoErros = (erro, req, res, next) => {
-    if (erro = mongoose.Error.CastError) {
+    if (erro instanceof mongoose.Error.CastError) {
         res.status(501).send("Muito erros detectado");
     } else {
         console.log("Algum erro diferente detectado");
