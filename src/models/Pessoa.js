729a0a064c8af3909953be1cf0db77c9c49162ed
Alexandre-Passos <alexandre15passos@gmail.com>
2025-07-04 10:05:34 -0300

    adicionando o default no Schema

--- src/models/Pessoa.js
+++ src/models/Pessoa.js
@@ -2,14 +2,16 @@ import mongoose from "mongoose";
 
 const EsquemaPessoa = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    nome: { type: String, required: [true, "O nome é obrigatório"] }, 
+    nome: { type: String, required: [true, "O nome é obrigatório"] },
     idade: { type: Number, required: [true, "A idade é obrigatória"] },
-    nascionalidade: { type: String, required: [true, "A nacionalidade é obrigatória"] },
-    sexo: { type: String },
+    nascionalidade: { type: String, default: "Nascionalidade não informada" },
+    sexo: { type: String, default: "Sexo não informado" },
     profissao: { type: String },
 
 }, { versionKey: false })
 
 const ModeloPessoa = mongoose.model("Pessoas", EsquemaPessoa);
+
+//required: [true, "A nacionalidade é obrigatória"]
 
 export default ModeloPessoa;
